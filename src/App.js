325e23dc0f2c67340e8901f91455e3a17616b5f9
cyxkat157 <svhbatb0n@gmail.com>
2023-11-07 12:10:37 +0800

    hiij l bnda

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
-import "./App.css";
+import Navbar from "./Navbar";
+import Home from "./Home";
 
 function App() {
-  return <div>mongol</div>;
+  return (
+    <div className="App">
+      <Navbar />
+      <Home />
+      <div className="Content"></div>
+    </div>
+  );
 }
 
 export default App;
